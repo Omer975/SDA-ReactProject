ffbbe0799a5ab8a8c5d88132cf73a7b305701066
Omer Sozcu <omerfsozcu@gmail.com>
2020-11-05 19:08:44 +0100

    Organize the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './assets/images/logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Package Tracking Viewer</h1>
+    </div>
+  );
+}
+
+export default App;
