ccd60a5fab8cd491ad1dfff49a6c1bc0e0a7fcae
Omer Sozcu <omerfsozcu@gmail.com>
2020-11-05 23:11:25 +0100

    getting informations from file is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import logo from './assets/images/logo.svg';
 import './css/App.css';
 
-function App() {
+//import others
+import information from "./information.json";
+
+export default function App() {
   
   return (
     <div className="App">
-      <h1>Package Tracking Viewer</h1>
+      
+      <div className="posts">
+        <h1>Package Tracking Viewer</h1>
+        { information.map(post => {
+          return(
+            <div key={ post.id} className="post">
+              <h4>id:{ post.id }</h4>
+              <p>Status:{ post.status }</p>
+              <p>ETA:{ post.eta }</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
+
